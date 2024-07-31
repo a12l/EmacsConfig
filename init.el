bca778a0172cd94bad8b33c148634997ac3590c4
Albin a12l Otterhäll <dev@a12l.xyz>
2024-08-01 00:15:32 +0200

    Emacs: Make the GUI more clean

--- init.el
+++ init.el
@@ -1,1 +1,8 @@
 (defconst my-pkgs-dir (expand-file-name "./packages" (file-name-directory load-file-name)))
+
+(use-package emacs
+  :config
+  (setopt inhibit-startup-screen t)
+  (setopt initial-scratch-message nil)
+  (menu-bar-mode -1)
+  (tool-bar-mode -1))
