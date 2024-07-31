4ba079909e0100d5c1e3e15c68418370997b391f
Albin a12l Otterhäll <dev@a12l.xyz>
2024-08-01 00:45:17 +0200

    Emacs: Auto save to visited file
    Don't create new files for any type of backup files.

--- init.el
+++ init.el
@@ -5,5 +5,8 @@
   (setopt inhibit-startup-screen t)
   (setopt initial-scratch-message nil)
   (setopt cursor-type 'bar)
+  (setopt make-backup-files nil)
+  (auto-save-mode -1)
+  (auto-save-visited-mode 1)
   (menu-bar-mode -1)
   (tool-bar-mode -1))
