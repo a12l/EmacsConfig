b418e13f30795b416448aeb87899db3fc89ddeef
Albin a12l Otterhäll <dev@a12l.xyz>
2024-09-11 11:04:54 +0200

    Emacs/auto-compile: intro at 5cc4e97

--- early-init.el
+++ early-init.el
@@ -1,1 +1,8 @@
+;;; -*- no-byte-compile: t -*-
+(setq load-prefer-newer t)
+(add-to-list 'load-path (expand-file-name "lib/auto-compile" user-emacs-directory))
+(require 'auto-compile)
+(auto-compile-on-load-mode)
+(auto-compile-on-save-mode)
+
 (setq package-enable-at-startup nil)
